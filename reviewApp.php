983f5d5b461d7ac619a6d323d2daf5ab64895be8
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-18 17:17:03 +0900

    配列使って見た。うまくいかない

--- reviewApp.php
+++ reviewApp.php
@@ -2,12 +2,23 @@
 <?php
 //変数定義
  $Line = "---------------------------\n";
- $type = "佐藤";
- $title = "佐藤";
- $comment = "佐藤";
+ $input_message = "plese input ";
+ // $type = "佐藤";
+ // $title = "佐藤";
+ // $comment = "佐藤";
+ $post = array();
+ //連想配列＝ハッシュ。添字配列＝配列？
+
+ echo $input_message . "type\n";
+ $post['type'] = trim(fgets(STDIN));
+ echo $input_message . "title\n";
+ $post['title'] = trim(fgets(STDIN));
+ echo $input_message . "comment\n";
+ $post['commnet'] = trim(fgets(STDIN));
 
 //output
-  echo "type: $type\n" . $Line;
-  echo "title: $title\n" . $Line;
-  echo "comment: $comment\n" . $Line;
+  var_dump($post);
+  echo "type: $post['type']\n" . $Line;
+  echo "title: $post['title']\n" . $Line;
+  echo "comment: $post['commnet']\n" . $Line;
  ?>
