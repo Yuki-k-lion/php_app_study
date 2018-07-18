4073d856b84447e77a2606bccd22ff6c0d93e8d0
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-18 17:04:51 +0900

    show comment

---
+++ reviewApp.php
@@ -0,0 +1,13 @@
+
+<?php
+//変数定義
+ $Line = "---------------------------\n";
+ $type = "佐藤";
+ $title = "佐藤";
+ $comment = "佐藤";
+
+//output
+  echo "type: $type\n" . $Line;
+  echo "title: $title\n" . $Line;
+  echo "comment: $comment\n" . $Line;
+ ?>
