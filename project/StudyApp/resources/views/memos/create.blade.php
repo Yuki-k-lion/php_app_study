9014c356ba26dd9304f8333157eae1e15d2695cf
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-25 10:45:46 +0900

    add craete view

---
+++ project/StudyApp/resources/views/memos/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+<div class="contents row">
+    <div class="container">
+        {{ Form::open(['url' => '/memos', 'method' => 'post']) }}
+            <h3>
+                    投稿する
+            </h3>
+            <input placeholder="Nickname" type="text" name="name">
+            <textarea cols="30" name="text" placeholder="text" rows="10"></textarea>
+            <input type="submit" value="SENT">
+        {{ Form::close() }}
+    </div>
+</div>
+@endsection
