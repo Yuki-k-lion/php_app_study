5b58051cb4d176e8900ec98efa7e7bc16a74ab29
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-29 12:18:27 +0900

    add delete pass on view file

--- project/StudyApp/resources/views/memos/index.blade.php
+++ project/StudyApp/resources/views/memos/index.blade.php
@@ -4,6 +4,10 @@
       <div class="contents row">
         @foreach($memos as $memo)
           <div class="content_post">
+            <ul class = "memo__list">
+              <li><a href="#">Edit</a>
+              <li><a href="memos/{{$memos->id}}/delete">Delete</a>
+            </ul>
             <p>{{ $memo->text  }}</p>
             <span class="name">{{ $memo->name }}</span>
           </div>
