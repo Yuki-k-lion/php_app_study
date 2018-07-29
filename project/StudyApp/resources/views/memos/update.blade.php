ee9ef00a697011d186f90f010b65b9550df45651
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-29 13:43:45 +0900

    edit update view file

---
+++ project/StudyApp/resources/views/memos/update.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+<div class="contents row">
+  <div class="success">
+    <h3>
+      success
+    </h3>
+    <a class="btn" href="/">Back to Memos</a>
+  </div>
+</div>
+@endsection
