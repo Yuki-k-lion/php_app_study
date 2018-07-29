cc8ef91d2d9973b45de644c4489904a338e1f68a
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-29 12:30:53 +0900

    add destroy view

---
+++ project/StudyApp/resources/views/memos/destroy.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+<div class="contents row">
+  <div class="success">
+    <h3>Deleted</h3>
+    <a class="btn" href="/">back to Memos</a>
+  </div>
+</div>
+@endsection
