4d554f41e42210c2d2ff68b1aac8ecc3f591b327
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-29 19:44:12 +0900

    edit styleing

--- project/StudyApp/resources/views/memos/index.blade.php
+++ project/StudyApp/resources/views/memos/index.blade.php
@@ -11,23 +11,26 @@
             <p>{{ $memo->text  }}</p>
             <span class="name">{{ $memo->name }}</span>
           </div> --}}
-          <div class="col s12 m7">
+          <div class="col s12">
             <div class="card horizontal">
               <div class="card-image">
                 <img src="https://lorempixel.com/100/190/nature/6">
               </div>
               <div class="card-stacked">
                 <div class="card-content">
-                  <span class="name">{{ $memo->name }}</span>
-                  <p>{{ $memo->text  }}</p>
+
+                    <h6 class="name">{{ $memo->name }}</h6>
+                    <p>{{ $memo->text  }}</p>
+                    <a href="/memos/{{$memo->id}}/edit" class="btn-floating halfway-fab accent-3 waves-effect waves-light cyan pulse"><i class="material-icons">edit</i></a>
+
                 </div>
                 <div class="card-action">
-                  <a href="/memos/{{$memo->id}}/edit">Edit</a>
                   <a href="/memos/{{$memo->id}}/delete">Delete</a>
                 </div>
               </div>
             </div>
           </div>
+
         @endforeach
       </div>
 @endsection
