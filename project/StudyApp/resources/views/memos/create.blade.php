e9eebbb178d66780d5c945a92d182fbf61b1b919
yuki komatsu <yuki.k.lion@gmail.com>
2018-07-29 20:42:58 +0900

    見た目も完成

--- project/StudyApp/resources/views/memos/create.blade.php
+++ project/StudyApp/resources/views/memos/create.blade.php
@@ -4,13 +4,26 @@
 <div class="contents row">
     <div class="container">
         {{ Form::open(['url' => '/memos', 'method' => 'post']) }}
-            <h3>
-                    投稿する
-            </h3>
-            <input placeholder="Nickname" type="text" name="name"></input>
-            <textarea cols="30" name="text" placeholder="text" rows="10"></textarea>
-            <input type="submit" value="SENT">
+        <h3 class="btn-large disabled">
+          Create
+        </h3>
+        <div class="row">
+           <div class="input-field col s6">
+             <input id="first_name2" type="text" class="validate" name="name" autofocus="true">
+             <label class="active" for="first_name2">First Name</label>
+           </div>
+           <div class="input-field col s12">
+             <textarea id="textarea1" class="materialize-textarea" name="text"></textarea>
+             <label for="textarea1">Textarea</label>
+           </div>
+         </div>
+
+            {{-- <input type="submit" value="SENT" class="waves-effect waves-light btn sent-white"> --}}
+            <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+              <i class="material-icons right">send</i>
+            </button>
         {{ Form::close() }}
     </div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 @endsection
--- project/StudyApp/resources/views/memos/edit.blade.php
+++ project/StudyApp/resources/views/memos/edit.blade.php
@@ -4,13 +4,24 @@
 <div class="contents row">
    <div class="container">
    {{ Form::open(['url' => "/memos/$memo->id", 'method' => 'PATCH'])}}
-     <h3>
+     <h3 class="btn-large disabled">
        Edit
      </h3>
-     <input placeholder="Nickname" type="text" name="name" value={{$memo->name}} autofocus="true"></input>
-     <textarea cols="30" name="text" placeholder="text" rows="10">{{$memo->text}}</textarea>
-     <input type="submit" value="SENT">
+     <div class="row">
+        <div class="input-field col s6">
+          <input id="first_name2" type="text" class="validate" name="name" value={{$memo->name}} autofocus="true">
+          <label class="active" for="first_name2">First Name</label>
+        </div>
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea" name="text">{{$memo->text}}</textarea>
+          <label for="textarea1">Textarea</label>
+        </div>
+      </div>
+      <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+        <i class="material-icons right">send</i>
+      </button>
    {{ Form::close() }}
    </div>
  </div>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 @endsection
